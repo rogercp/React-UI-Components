d862a10662ed225e392bcb1e611c65faed0b66d4
Roger Caballero <rogercp1234@yahoo.com>
2019-04-23 12:19:55 -0700

    display button no css

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay=()=>{
+    return (
+        <div>
+            <button className="calc-display">0</button>
+        </div>
+    )
+}
+export default CalculatorDisplay;
